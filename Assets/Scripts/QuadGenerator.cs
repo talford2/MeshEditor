3ddf44dfde8fef4b84ded5c35b1100b4f6ad4357
Timothy <talford2@hotmail.com>
2016-02-26 01:18:22 +1100

    Simplifying and adding more functionality

--- Assets/Scripts/QuadGenerator.cs
+++ Assets/Scripts/QuadGenerator.cs
@@ -1,45 +1,53 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class QuadGenerator : MonoBehaviour
 {
 	public MeshFilter MeshFilter;
 	
+	public bool RelativeToWorld = false;
+
+	public float UVScale = 1f;
+
+	public Vector2 Offset = Vector2.zero;
+
 	void Awake()
 	{
 		var mesh = new Mesh();
-		var vert = new List<Vector3> {
+
+		mesh.vertices = new Vector3[] {
 			new Vector3(0, 0, 0),
 			new Vector3(1, 0, 0),
 			new Vector3(1, 0, 1),
 			new Vector3(0, 0, 1)
 		};
-		mesh.vertices = vert.ToArray();
-		
-		var uvs = new List<Vector2> {
-			new Vector2(0,0),
-			 new Vector2(transform.localScale.x,0),
-			 new Vector2(transform.localScale.x,transform.localScale.z),
-			 new Vector2(0,transform.localScale.z)
-		};
-		mesh.uv = uvs.ToArray();
 
-		var tris = new List<int> { 2, 1, 0, 3, 2, 0 };
-		mesh.triangles = tris.ToArray();
+		mesh.triangles = new int[] { 2, 1, 0, 3, 2, 0 };
 
-		var norms = new List<Vector3> { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
-		mesh.normals = norms.ToArray();
+		mesh.normals = new Vector3[] { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
 
 		MeshFilter.mesh = mesh;
+		SetUVs();
 	}
 
 	void Update()
 	{
-		MeshFilter.mesh.uv = new List<Vector2> {
-			new Vector2(0,0),
-			 new Vector2(transform.localScale.x,0),
-			 new Vector2(transform.localScale.x,transform.localScale.z),
-			 new Vector2(0,transform.localScale.z)
-		}.ToArray();
+		SetUVs();
+	}
+
+	private void SetUVs()
+	{
+		var offset = Offset;
+
+		if (RelativeToWorld)
+		{
+			offset = new Vector2(Offset.x + transform.position.x, Offset.y + transform.position.z);
+		}
+
+		MeshFilter.mesh.uv = new Vector2[] {
+			new Vector2(offset.x, offset.y),
+			new Vector2(offset.x+ transform.localScale.x * UVScale, offset.y),
+			new Vector2(offset.x+ transform.localScale.x * UVScale, offset.y+ transform.localScale.z * UVScale),
+			new Vector2(offset.x, offset.y + transform.localScale.z * UVScale)
+		};
 	}
 }
