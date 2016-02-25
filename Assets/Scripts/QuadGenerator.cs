6dd61c7a8033692b5d55048ff2d7a676534a447a
Timothy <talford2@hotmail.com>
2016-02-26 00:46:54 +1100

    Making UV repeat when scaled

--- Assets/Scripts/QuadGenerator.cs
+++ Assets/Scripts/QuadGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class QuadGenerator : MonoBehaviour
 {
 	public MeshFilter MeshFilter;
-
+	
 	void Awake()
 	{
 		var mesh = new Mesh();
@@ -15,18 +15,18 @@ public class QuadGenerator : MonoBehaviour
 			new Vector3(0, 0, 1)
 		};
 		mesh.vertices = vert.ToArray();
-
+		
 		var uvs = new List<Vector2> {
 			new Vector2(0,0),
-			 new Vector2(1,0),
-			 new Vector2(1,1),
-			 new Vector2(0,1)
+			 new Vector2(transform.localScale.x,0),
+			 new Vector2(transform.localScale.x,transform.localScale.z),
+			 new Vector2(0,transform.localScale.z)
 		};
 		mesh.uv = uvs.ToArray();
 
 		var tris = new List<int> { 2, 1, 0, 3, 2, 0 };
 		mesh.triangles = tris.ToArray();
-		
+
 		var norms = new List<Vector3> { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
 		mesh.normals = norms.ToArray();
 
@@ -35,6 +35,11 @@ public class QuadGenerator : MonoBehaviour
 
 	void Update()
 	{
-
+		MeshFilter.mesh.uv = new List<Vector2> {
+			new Vector2(0,0),
+			 new Vector2(transform.localScale.x,0),
+			 new Vector2(transform.localScale.x,transform.localScale.z),
+			 new Vector2(0,transform.localScale.z)
+		}.ToArray();
 	}
 }
