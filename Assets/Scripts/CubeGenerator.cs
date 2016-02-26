b1ee2a67609b937a744f0921ae07f71fd6778ae4
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-26 15:08:09 +0800

    Add new component for cubes.

---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class CubeGenerator : MonoBehaviour {
+    public MeshFilter MeshFilter;
+
+    public bool FrontCap = true;
+
+    public bool BackCap = true;
+
+    public bool RelativeToWorld = false;
+
+    public float UVScale = 1f;
+
+    public Vector3 Offset = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GenerateMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        var mesh = new Mesh();
+
+        mesh.vertices = new Vector3[] {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1),
+            new Vector3(0, 0, 1),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(1, 1, 1),
+            new Vector3(0, 1, 1)
+        };
+
+        var tris = new List<int>() {
+            0,4,5,0,5,1,
+            4,7,6,4,6,5,
+            3,6,7,3,2,6
+        };
+
+        if (FrontCap)
+        {
+            tris.AddRange(new List<int> {
+                3,7,4,
+                0,3,4
+            });
+        }
+
+        if (BackCap)
+        {
+            tris.AddRange(new List<int> {
+                1,5,6,
+                1,6,2
+            });
+        }
+
+        mesh.triangles = tris.ToArray();
+
+        mesh.normals = new Vector3[] {
+            Vector3.back,
+            Vector3.back,
+
+            Vector3.forward,
+            Vector3.forward,
+
+            Vector3.up,
+            Vector3.up,
+
+            Vector3.up,
+            Vector3.up
+        };
+
+        MeshFilter.sharedMesh = mesh;
+
+        var offset = Offset;
+
+        if (RelativeToWorld)
+        {
+            offset += transform.position;
+        }
+
+        var x0 = offset.x * UVScale;
+        var y0 = offset.y * UVScale;
+        var z0 = offset.z * UVScale;
+
+        var x1 = offset.x * UVScale + transform.localScale.x * UVScale;
+        var y1 = offset.y * UVScale + transform.localScale.y * UVScale;
+        var z1 = offset.z * UVScale + transform.localScale.z * UVScale;
+
+        MeshFilter.sharedMesh.uv = new Vector2[]
+        {
+            new Vector2(x0, y0),
+            new Vector2(x1, y0),
+            new Vector2(x1, y1),
+            new Vector2(x1, y0),
+
+            new Vector2(x0, z1),
+            new Vector2(x1, z1),
+            new Vector2(x1, z0),
+            new Vector2(x0, z0)
+        };
+    }
+}
