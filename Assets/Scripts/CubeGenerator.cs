8d382f3bac816811e8a54ac385391cb130021e01
Steven Alford <steven.alford@collinsonlatitude.com>
2016-02-26 17:05:19 +0800

    Cube progress.

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -29,53 +29,44 @@ public class CubeGenerator : MonoBehaviour {
     {
         var mesh = new Mesh();
 
+        var verts = new Vector3[]
+        {
+            new Vector3(-0.5f, 0.5f, 0.5f),
+            new Vector3(0.5f, 0.5f, 0.5f),
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(-0.5f, -0.5f, 0.5f),
+
+            new Vector3(-0.5f, 0.5f, -0.5f),
+            new Vector3(0.5f, 0.5f, -0.5f),
+            new Vector3(0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, -0.5f)
+        };
+
         mesh.vertices = new Vector3[] {
-            new Vector3(0, 0, 0),
-            new Vector3(1, 0, 0),
-            new Vector3(1, 0, 1),
-            new Vector3(0, 0, 1),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 1, 0),
-            new Vector3(1, 1, 1),
-            new Vector3(0, 1, 1)
+            // Front
+            verts[0], verts[1], verts[2], verts[3], // 0, 1, 2, 3
+            // Top
+            verts[0], verts[4], verts[5], verts[1], // 4, 5, 6, 7
+            // Back
+            verts[4], verts[5], verts[6], verts[7], // 8, 9, 10, 11
         };
 
-        var tris = new List<int>() {
-            0,4,5,0,5,1,
-            4,7,6,4,6,5,
-            3,6,7,3,2,6
+        var tris = new int[]
+        {
+            // front
+            0, 3, 2, 2, 1, 0,
+            // top
+            4, 7, 6, 6, 5, 4,
+            // back
+            8, 9, 10, 10, 11, 8
         };
 
-        if (FrontCap)
-        {
-            tris.AddRange(new List<int> {
-                3,7,4,
-                0,3,4
-            });
-        }
-
-        if (BackCap)
-        {
-            tris.AddRange(new List<int> {
-                1,5,6,
-                1,6,2
-            });
-        }
-
-        mesh.triangles = tris.ToArray();
+        mesh.triangles = tris;
 
         mesh.normals = new Vector3[] {
-            Vector3.back,
-            Vector3.back,
-
-            Vector3.forward,
-            Vector3.forward,
-
-            Vector3.up,
-            Vector3.up,
-
-            Vector3.up,
-            Vector3.up
+            Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.up, Vector3.up, Vector3.up, Vector3.up,
+            Vector3.back, Vector3.back, Vector3.back, Vector3.back
         };
 
         MeshFilter.sharedMesh = mesh;
@@ -87,25 +78,31 @@ public class CubeGenerator : MonoBehaviour {
             offset += transform.position;
         }
 
-        var x0 = offset.x * UVScale;
-        var y0 = offset.y * UVScale;
-        var z0 = offset.z * UVScale;
+        var x0 = (offset.x - 0.5f * transform.localScale.x) * UVScale;
+        var y0 = (offset.y + 0.5f * transform.localScale.y) * UVScale;
+        var z0 = (offset.z + 0.5f * transform.localScale.z) * UVScale;
 
-        var x1 = offset.x * UVScale + transform.localScale.x * UVScale;
-        var y1 = offset.y * UVScale + transform.localScale.y * UVScale;
-        var z1 = offset.z * UVScale + transform.localScale.z * UVScale;
+        var x1 = (offset.x + 0.5f * transform.localScale.x) * UVScale;
+        var y1 = (offset.y - 0.5f * transform.localScale.y) * UVScale;
+        var z1 = (offset.z - 0.5f * transform.localScale.z) * UVScale;
 
         MeshFilter.sharedMesh.uv = new Vector2[]
         {
+            // front
             new Vector2(x0, y0),
             new Vector2(x1, y0),
             new Vector2(x1, y1),
+            new Vector2(x0, y1),
+            // top
+            new Vector2(z0, x0),
+            new Vector2(z1, x0),
+            new Vector2(z1, x1),
+            new Vector2(z0, x1),
+            // back
+            new Vector2(x0, y0),
             new Vector2(x1, y0),
-
-            new Vector2(x0, z1),
-            new Vector2(x1, z1),
-            new Vector2(x1, z0),
-            new Vector2(x0, z0)
+            new Vector2(x1, y1),
+            new Vector2(x0, y1),
         };
     }
 }
